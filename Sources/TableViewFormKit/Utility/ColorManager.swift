40fb80a27489b4cd72e54e82f9f25f9bd2aadeea
Vikram Singh <vik.devmail@gmail.com>
2023-05-15 02:14:04 -0400

    C: ColorManager, cleanup

---
+++ Sources/TableViewFormKit/Utility/ColorManager.swift
@@ -0,0 +1,19 @@
+//
+//  ColorManager.swift
+//  
+//
+//  Created by Vikram Singh on 5/14/23.
+//
+
+import Foundation
+import UIKit
+
+public final class ColorManager {
+    public static var primaryTextColor: UIColor = .label
+    public static var primaryAccentColor: UIColor = .link
+    
+    public static var primaryBackgroundColor: UIColor = .systemBackground
+    public static var secondaryBackgroundColor: UIColor = .secondarySystemBackground
+    
+    public static var failure: UIColor = .red
+}
