c977ab61b5c6d7a03d22a32c0b857b7c4f39e7bb
Vikram Singh <vik.devmail@gmail.com>
2023-05-15 02:13:15 -0400

    NC: Implement FormCell

---
+++ Sources/TableViewFormKit/FormCell.swift
@@ -0,0 +1,117 @@
+//
+//  FormCell.swift
+//
+//
+//  Created by Vikram Singh on 5/14/23.
+//
+import Foundation
+import UIKit
+import VikUtilityKit
+
+/// Form cells and all their necessary information to build a form
+public enum FormCell: Equatable {
+    public static func == (lhs: FormCell, rhs: FormCell) -> Bool {
+        return "\(lhs)" == "\(rhs)"
+    }
+    
+    case textFieldCell(
+        placeholderText: String,
+        text: String?,
+        id: FormCellID.TextFieldCellID,
+        textFieldDelegate: UITextFieldDelegate,
+        delegate: UITextFieldDelegateExtension
+    )
+    
+    case switchCell(
+        cellText: String,
+        isOn: Bool,
+        switchDelegate: CanHandleSwitch?,
+        infoDelegate: CanHandleInfoDisplay?
+    )
+    
+    case labelCell(
+        cellText: String,
+        textColor: UIColor = ColorManager.primaryTextColor,
+        backgroundColor: UIColor = ColorManager.secondaryBackgroundColor,
+        cellAccessoryType: UITableViewCell.AccessoryType = .none,
+        onClick: (() -> Void)? = nil
+    )
+    
+    case errorCell(
+        errors: [any FormError]
+    )
+    
+    case timeCell(
+        cellText: String,
+        date: Date,
+        dateFormat: DateFormat,
+        timePickerMode: UIDatePicker.Mode,
+        id: FormCellID.TimeCellID,
+        onClick: ((IndexPath) -> Void)?
+    )
+    
+    case timePickerCell(
+        date: Date,
+        dateFormat: DateFormat,
+        timePickerMode: UIDatePicker.Mode,
+        id: FormCellID.TimePickerCellID,
+        delegate: UITimePickerDelegate
+    )
+    
+    case daySelectorCell(
+        daysSelected: Set<Weekday>,
+        delegate: DaySelectorDelegate
+    )
+    
+    case segmentedControlCell(
+        firstTitle: String,
+        secondTitle: String,
+        delegate: SegmentedControlDelegate
+    )
+    
+    case colorPickerCell(delegate: ColorDelegate)
+    
+    case pickerCell(
+        cellText: String,
+        indices: [Int],
+        tag: FormCellID.PickerCellID,
+        delegate: UIPickerViewDelegate,
+        dataSource: UIPickerViewDataSource
+    )
+    
+    case logoCell(
+        logo: SystemIcon,
+        onClick: (() -> Void)? = nil
+    )
+}
+
+/// IDs for FormCells that we can use instead of hardcoded strings
+public enum FormCellID {
+    
+    // TextField Cells
+    public enum TextFieldCellID {
+        case nameTextField
+        case locationTextField
+        case additionalDetailsTextField
+    }
+    
+    // TimePickerCells
+    public enum TimePickerCellID {
+        case startDateTimePicker
+        case endDateTimePicker
+        case lengthTimePicker
+    }
+    
+    // TimeCells
+    public enum TimeCellID {
+        case startTimeCell
+        case endTimeCell
+        case lengthTimeCell
+    }
+    
+    // PickerCells need to be Ints since we use tag
+    public enum PickerCellID: Int {
+        case coursePickerCell = 0
+        case lengthPickerCell = 1
+    }
+}
